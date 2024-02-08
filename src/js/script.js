55d66130309a34ca314471f2f239ee0e80a05255
Oleg Sazonov <sazonov1994@gmail.com>
2024-02-08 21:24:06 +0200

    add lastMovie, rate into numberOfFilms

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,16 @@
 'use strict';
 
 let numberOfFilms = prompt('How many films you have already watched?', '');
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+let lastMovie = prompt('What film you had watched recently?', '');
+let rate = prompt('How would you rate it?', '');
+
+personalMovieDB['movies'] = {
+    lastMovie: rate
+};
